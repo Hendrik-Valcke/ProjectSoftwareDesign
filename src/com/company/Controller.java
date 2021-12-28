35bade7b3f290dc8437bc8e0823ce7eb3c98b2cd
Hendrik Valcke <hendrik.valcke@hotmail.be>
2021-12-28 18:47:37 +0100

    comments

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -13,11 +13,39 @@ public class Controller {
 
     public void start()
     {
+        System.out.println("controller:start");
         view.showStartScreen();
+        view.setVisible(true);
     }
-//basically alle buttons roepen een functie op van controller
-    public void login()
+
+//basically all buttons call a function of the controller
+    public void startLogin()
     {
+        //triggered when the user presses 'login'in startscreen or 'change user' in homescreen
+        //the user will enter info and press the login button
         view.showLoginScreen();
+        view.setVisible(true);
+
     }
+
+    public void checkLogin(String username,String password)
+    {
+        //triggered when someone presses 'login' after entering info in loginScreen
+
+        //a function that looks for the username in the database and confirms whether the password is correct
+        //
+        //if the check returns positive:
+            //send to homescreen
+            //Home();
+        //if the check returns negative:
+            //make a messagebox:error wrong info...
+            //startLogin()
+    }
+
+    /*public void startcreateNewUser()
+    {
+        view.showCreateUser();
+    }*/
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("testing main:");
+        Main main = new Main();
+        main.run();
         /*
         Window mainWindow = new Window();
         mainWindow.showCustomTicketScreen();
@@ -35,15 +37,27 @@ public class Main {
     void run()
     {
         ////=mvc logica
+        /*the controller handles the traffic between the gui(view) and the data(model)
+        example: user presses the 'caculate'button in the homescreen
+                --> the button calls the function 'calculateTotal' in the controller
+                --> calculateTotal calls a function from the model, which performs the calculation using the databases
+                --> the function returns an answer to the controller
+                --> the controller calls: view.showEndresult() which displays the values everyone has to pay to each other
+                (de helft van deze functies bestaan nog ni )
+        */
+
         ////maak db aan
+        System.out.println("make model");
         Model model= new Model(TicketDatabase.getInstance(),UserDatabase.getInstance());
 
         ////maak window aan
+        System.out.println("make view");
         Window view = new Window();
 
         ////maak controller aan
+        System.out.println("make controller");
         Controller controller= new Controller(model,view);
-        
+
         // begin in startscreen, knoppen bepalen volgende stappen
         controller.start();
 
