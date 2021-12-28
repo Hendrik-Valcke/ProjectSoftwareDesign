bf2982a6ce653c4dc975f168bd7c3d13ac8f9196
Hendrik Valcke <hendrik.valcke@hotmail.be>
2021-12-28 16:43:12 +0100

    gui changes

--- src/com/company/GUI/subframes/CustomTicketScreen.java
+++ src/com/company/GUI/subframes/CustomTicketScreen.java
@@ -6,10 +6,13 @@ public class CustomTicketScreen implements SubFrame{
     private JPanel rootPanel;
     private JLabel titleLabel;
     private JTextField textField2;
-    private JTextField textField3;
     private JTextField textField1;
     private JButton cancelButton;
     private JButton saveTicketButton;
+    private JList userList;
+    private JLabel listLabel;
+    private JButton saveShareButton;
+    private JTextField shareField;
 
     @Override
     public JPanel getRootPanel() {
