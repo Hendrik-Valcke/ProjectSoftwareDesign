e6ab7946a1d83ed09954f5af8a5f1a23e8daf4d2
ilias <ilibtwee@gmail.com>
2022-01-14 23:58:12 +0100

    filesystem changes to fit tests

---
+++ src/test/java/unit_tests/EvenTicket_UTest.java
@@ -0,0 +1,23 @@
+package unit_tests;
+
+import com.company.tickets.EvenTicket;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class EvenTicket_UTest {
+    public EvenTicket_UTest() {
+    }
+
+    @Before
+    public void initialize() {
+
+    }
+
+    @Test
+    public void t_getEvent() {
+        EvenTicket evenTicketUnderTest  ;
+
+    }
+}
---
+++ src/test/java/unit_tests/User_UTest.java
@@ -0,0 +1,18 @@
+package unit_tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class User_UTest {
+    public User_UTest()
+    {
+
+    }
+    @Before
+    public void initialize()
+    {
+
+    }
+
+
+}
